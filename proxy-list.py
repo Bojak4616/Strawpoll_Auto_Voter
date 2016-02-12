7dcf4776dc8d2cea3b47fb063f1c708966d6f8d3
Jesse Buonanno <jpb4616@rit.edu>
2016-02-12 13:08:17 -0500

    Initial Commit

---
+++ proxy-list.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import requests
+from bs4 import BeautifulSoup
+
+if __name__=="__main__":
+	r = requests.get("http://free-proxy-list.net/")
+	
+	ip = []
+	port = []
+	https = []
+	count = 0
+	soup = BeautifulSoup(r.content, 'html.parser')
+	for coulmn in soup.find_all('td'):
+		count +=1
+		if count == 1:
+			ip.append(coulmn.text)
+
+		if count == 2:
+			port.append(coulmn.text)
+
+		if count == 7:
+			https.append(coulmn.text)
+		
+		if count == 8:
+			count = 0
+
+	with open("proxy.csv", 'r') as CSV:
+		allText = CSV.read()
+	
+	with open("proxy.csv", 'a') as CSV:
+		for num in range(0, len(ip)):
+			if allText.find(ip[num]) != -1:
+				continue
+
+			CSV.write(ip[num] + ',' + str(port[num]) + ',' + https[num] + '\n')
---
+++ strawpoll.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import requests
+import threading
+import sys
+from random import randint
+
+def worker(proxyDict):
+	userAgents = ["Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36", 
+				"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"]
+
+	headers = {'User-Agent' : userAgents[randint(0,2)]}
+	try:
+		#proxies=proxyDict,
+		r = requests.get("http://strawpoll.me/6770460", headers=headers)
+		cookies = {'__cfduid': r.cookies["__cfduid"]}
+			
+		vote = []
+		vote.append(2)
+		r = requests.post("http://strawpoll.me/api/v2/votes", headers=headers, cookies=cookies, proxies=proxyDict, json={"pollId":"6770460", "votes": vote})
+	except requests.exceptions.RequestException:
+		global badProxies
+		badProxies.append(proxyDict)
+	
+	print r.status_code
+
+
+if __name__ == '__main__':
+	badProxies = []
+
+	with open('proxy.csv') as CSV:
+		data = CSV.read()
+
+	line = data.split('\n')
+	numProxies = len(line)
+
+	for num in range(0,numProxies-1):
+		proxy = line[num].split(',')
+		
+		if proxy[2] == 'yes':
+			https = 'https'
+		else:
+			https = 'http'
+
+		address = "http://" + proxy[0] + ':' + proxy[1]
+		proxyDict = {https : address}
+
+		t = threading.Thread(target=worker, args=(proxyDict,))
+		t.start()	
+
+	while(True):
+		if threading.activeCount() == 1:
+			print "---------------------------------------------------------------"
+			for bad in badProxies:
+				print bad
+			print "Done"
+			sys.exit(0)
